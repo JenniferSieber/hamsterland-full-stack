6e47fd541bf128a76f6ff10f902ec724674dcf5c
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-03-15 17:10:55 -0600

    Add files via upload

---
+++ models/Item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  }
+})
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3001
+
+// connect to mongodb
+mongoose.connect(`${process.env.DB_URI}`)
+  .then(() => console.log(`MongoDB Connected`))
+  .catch(err => console.log(`MongoDB error in connection: ${err}`))
+
+  //app middleware
+  app.use(cors())
+  app.use(express.static('public'))
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.json())
+  app.set('view engine', 'ejs')
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+  app.listen(port, () => console.log(`Server connected port: ${port}`))
+
